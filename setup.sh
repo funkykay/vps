815ff9a76ea0bdf6811418e3166e98942d5443f9
Kay Donner <kay.donner@web.de>
2025-07-31 04:20:15 +0000

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+SSH_PORT="${SSH_PORT:-2222}"
+echo "SSH-Port wird auf ${SSH_PORT} gesetzt"
+
+apt update && apt upgrade -y
+
+apt install -y ufw
+ufw allow "${SSH_PORT}/tcp"
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw --force enable
+
+sed -i "s/^#Port 22/Port ${SSH_PORT}/" /etc/ssh/sshd_config
+systemctl restart ssh
